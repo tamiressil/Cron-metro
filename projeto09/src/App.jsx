0754b775eed21d40bdb8f6be920f3b34e4e78cbf
tamiressil <tamiressilvabezerra05@gmail.com>
2024-05-20 12:18:34 -0300

    primeiro commit

---
+++ projeto09/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Cronometro from './Componentes/Cronômetro'
+import './App.css'
+
+export default function App() {
+  return (
+    <div className='App'>
+      <Cronometro/>
+    </div>
+  )
+}
